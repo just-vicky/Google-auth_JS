4d5d6ab55f4af5c4359bf9ea9fa6e063c5ba7698
Vikram <vikraman@auroville.org.in>
2023-12-14 20:23:00 +0530

    Decrypt email and authorise

--- script.js
+++ script.js
@@ -1,33 +1,63 @@
-//Function to encrypt emails
-// function encryptEmail(email) {
-//     // Use Base64 encoding for simplicity (not secure for sensitive data)
-//     const encryptedEmail = btoa(email);
+
+
+// const usersArray = [
+//     {
+//       email: "vikramauroville2000@gmail.com",
+//     },
+//     {
+//       email: "user2@example.com",
+//     },
+//     {
+//       email: "user3@example.com",
+//     }
+//   ];
+  
+//   // Function to generate a random key
+//   function generateRandomKey() {
+//     return CryptoJS.lib.WordArray.random(16).toString(CryptoJS.enc.Hex);
+//   }
+  
+//   // Function to encrypt email using AES encryption
+//   function encryptEmail(email, key) {
+//     const encryptedEmail = CryptoJS.AES.encrypt(email, key).toString();
 //     return encryptedEmail;
 //   }
   
-//   const emailList = ['vikramauroville2000@gmail.com', 'anotherauthorized@example.com'];
-  
-//   const encryptedEmailList = emailList.map(email => encryptEmail(email));
-  
-//   console.log(encryptedEmailList);
-  
+//   // Encrypt email for each user in the array
+//   const encryptedUsersArray = usersArray.map(user => {
+//     const key = generateRandomKey();
+//     return {
+//       email: encryptEmail(user.email, key),
+//       key: key
+//     };
+//   });
 
-const enclist = ["dmlrcmFtYXVyb3ZpbGxlMjAwMEBnbWFpbC5jb20=", "YW5vdGhlcmF1dGhvcml6ZWRAZXhhbXBsZS5jb20="];
-
-function decryptEmail(encryptedEmail) {
-    // Use Base64 decoding for simplicity (not secure for sensitive data)
-    const decryptedEmail = atob(encryptedEmail);
+// Function to decrypt email using AES decryption
+function decryptEmail(encryptedEmail, key) {
+    const decryptedBytes = CryptoJS.AES.decrypt(encryptedEmail, key);
+    const decryptedEmail = decryptedBytes.toString(CryptoJS.enc.Utf8);
     return decryptedEmail;
   }
-  
- 
-  const decryptedEmailList = enclist.map(encryptedEmail => decryptEmail(encryptedEmail));
-  
-  console.log(decryptedEmailList);
-  
 
+  function decryptUsers(encryptedUsersArray) {
+    const decryptedUsersArray = encryptedUsersArray.map(encryptedUser => {
+      const decryptedEmail = decryptEmail(encryptedUser.email, encryptedUser.key);
+      return {
+        email: decryptedEmail,
+        key: encryptedUser.key
+      };
+    });
 
-function signInWithGoogle() {
+    return decryptedUsersArray;
+  }
+
+  // Function to check if email is authorized
+  function checkAuthorizedEmail(email, decryptedUsersArray) {
+    return decryptedUsersArray.some(user => user.email === email);
+  }
+
+  // Function to initiate Google Sign-In
+  function signInWithGoogle() {
     let clientId = '694571191095-vabs5hp6qsui4rmc8ook40u3rku8r7he.apps.googleusercontent.com';
     let redirectUri = encodeURIComponent(window.location.origin);
 
@@ -39,59 +69,71 @@ function signInWithGoogle() {
         '&prompt=select_account';
 
     window.location.href = authUrl;
-}
+  }
 
-function checkAuthorizedEmail(email) {
-    // Use the decrypted email list for authorization
-    return decryptedEmailList.includes(email);
-}
-
-function handleSignIn() {
+  function handleSignIn() {
     let accessToken = window.location.hash.substr(1)
-        .split('&')[0]
-        .split('=')[1];
-
-    console.log(accessToken )
-
+      .split('&')[0]
+      .split('=')[1];
+  
     // Fetch user info using the access token
     fetch('https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + accessToken)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Unauthorized');
-            }
-            return response.json();
-        })
-        .then(data => {
-            // Log the entire response JSON in the console
-            console.log('Google API Response:', data);
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Unauthorized');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Log the entire response JSON in the console
+        console.log('Google API Response:', data);
+  
+        let encodedEmail = data.email;
+  
+        // Display the encoded email (you can modify this part as needed)
+        alert("Encoded Email: " + encodedEmail);
+  
+        // Assuming decryptedUsersArray is an array of objects with an 'email' property
+        console.log('Decrypted Users Array:', decryptedUsersArray);
+        if (checkAuthorizedEmail(encodedEmail, decryptedUsersArray)) {
+          // If the user is authorized, remove overlay and display content
+          document.getElementById('overlay').style.display = 'none';
+          document.getElementById('content').style.display = 'block';
+          alert("User has been authorized");
+        } else {
+          alert('Unauthorized email address.');
+          // If unauthorized, redirect back to the sign-in page
+          window.location.href = window.location.origin;
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching user info:', error);
+        // If an error occurs, redirect back to the sign-in page
+        window.location.href = window.location.origin;
+      });
+  }
+  
 
-            let encodedEmail = data.email;
+  // Fetch JSON file and decrypt before handling sign-in
+  fetch('https://raw.githubusercontent.com/just-vicky/Mock-Json/main/MOCK_DATA%20(1).json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Decrypt users and store in a variable
+      decryptedUsersArray = decryptUsers(data);
 
-            // Display the encoded email (you can modify this part as needed)
-            alert("Encoded Email: " + encodedEmail);
-
-            if (checkAuthorizedEmail(encodedEmail)) {
-                // Useconst enclist = ["dmlrcmFtYXVyb3ZpbGxlMjAwMEBnbWFpbC5jb20=", "YW5vdGhlcmF1dGhvcml6ZWRAZXhhbXBsZS5jb20="];r is authorized, remove overlay and display content
-                document.getElementById('overlay').style.display = 'none';
-                document.getElementById('content').style.display = 'block';
-                alert("User has been authorized");
-            } else {
-                alert('Unauthorized email address.');
-                // If unauthorized, redirect back to the sign-in page
-                window.location.href = window.location.origin;
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching user info:', error);
-            // If an error occurs, redirect back to the sign-in page
-            window.location.href = window.location.origin;
-        });
-}
-
-
-if (window.location.hash.includes('access_token')) {
-    handleSignIn();
-} else {
-    // If no access token, show the overlay
-    document.getElementById('overlay').style.display = 'block';
-}
+      // Call handleSignIn after decrypting users
+      if (window.location.hash.includes('access_token')) {
+        handleSignIn();
+      } else {
+        // If no access token, show the overlay
+        document.getElementById('overlay').style.display = 'block';
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching or decrypting data:', error);
+    });
